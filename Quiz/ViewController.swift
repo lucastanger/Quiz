574a7397b4a2e0aa623a0d2287469b717031d3ec
Luca Stanger <lstanger@gmx.de>
2020-03-02 14:00:42 +0100

    K01 finished

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel : UILabel!
+    @IBOutlet var answerLabel : UILabel!
+    
+    let questions: [String] = [
+        "What is 6x7?",
+        "How much wood would a woodchuck chuck?",
+        "What is brown and sticky"
+    ]
+    
+    let answers: [String] = [
+        "42. The Answer to life & universe & everything",
+        "As much as he could",
+        "A stick"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+
+
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender:UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-
 }
 
